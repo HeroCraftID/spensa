7a50f17b129b0a47c0a4b313c03ff1935b057fb2
HeroCraftID01 <153174570+HeroCraftID01@users.noreply.github.com>
2025-06-06 08:46:26 +0700

    a

--- Client/next.config.mjs
+++ Client/next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Enable environment variables to be available on the client
-  // This is needed for our config to work on the client side
   env: {
     ACCESS_START_HOUR: process.env.ACCESS_START_HOUR,
     ACCESS_START_MINUTE: process.env.ACCESS_START_MINUTE,
@@ -18,6 +17,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: "/client/education/spensa/998213/absen",
+  trailingSlash: true,
 };
 
 export default nextConfig;
--- admin/next.config.mjs
+++ admin/next.config.mjs
@@ -9,6 +9,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: "/admin/education/spensa/821022/absen",
+  trailingSlash: true,
   async headers() {
     return [
       {
