32735144c3a211c602c4df97349e1bbc6de04817
HeroCraftID01 <153174570+HeroCraftID01@users.noreply.github.com>
2025-06-08 21:53:58 +0700

    Update page.tsx

--- admin/app/page.tsx
+++ admin/app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from "next/navigation"
 
 export default function Home() {
   // Redirect to login page
-  redirect("/admin/education/spensa/821022/absen/main/login")
+  redirect("/main/login")
 }
